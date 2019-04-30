04037c0026ca3b869fb854ee8249c6ab0cc20aba
bstringham <bstringham@stuy.edu>
2019-04-30 11:01:24 -0400

    Finished the private classes

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,25 @@
 public class MyHeap {
-	private static void pushDown(int[] heap, int size, int index) {
-		if (index * 2 + 2 <= size && (heap[index] < heap[index * 2 + 1] || heap[index] ) {
+	private static void pushDown(int[] heap, int index) {
+		if (index * 2 + 2 < heap.length && (heap[index] < heap[index * 2 + 1] || heap[index] < heap[index * 2 + 2])) {
 			int value = heap[index];
-			int swapper;
+			int swapper = index * 2;
 			if (heap[index * 2 + 1] < heap[index * 2 + 2]) {
-				swapper = index * 2
+				swapper += 1;
+			} else {
+				swapper += 2;
+			}
+			heap[index] = heap[swapper];
+			heap[swapper] = value;
+			pushDown(heap, swapper);
+		}
+	}
+
+	private static void pushUp(int[] heap, int index) {
+		if (index > 0 && (heap[index] > heap[index / 2]) {
+			int value = heap[index];
+			heap[index] = heap[index / 2];
+			heap[index / 2] = value;
+			pushUp(heap, index / 2);
+		}
+	}
+}
