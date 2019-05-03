2de84e5d65d51d84955ae69a4567280e10189a5f
Innertuber <bstringham@stuy.edu>
2019-05-03 01:08:03 -0400

    Fixed pushDown, finished heapify

--- MyHeap.java
+++ MyHeap.java
@@ -1,25 +1,43 @@
 public class MyHeap {
 	private static void pushDown(int[] heap, int index) {
-		if (index * 2 + 2 < heap.length && (heap[index] < heap[index * 2 + 1] || heap[index] < heap[index * 2 + 2])) {
-			int value = heap[index];
+		if (index * 2 + 2 < heap.length && (heap[index] < heap[index * 2 + 1] || heap[index] < heap[index * 2 + 2]) || index * 2 + 1 < heap.length && heap[index] < heap[index * 2 + 1]) {
 			int swapper = index * 2;
-			if (heap[index * 2 + 1] < heap[index * 2 + 2]) {
+			if (index * 2 + 2 == heap.length || heap[index * 2 + 1] > heap[index * 2 + 2]) {
 				swapper += 1;
 			} else {
 				swapper += 2;
 			}
-			heap[index] = heap[swapper];
-			heap[swapper] = value;
+			swap(heap, index, swapper);
 			pushDown(heap, swapper);
 		}
 	}
 
 	private static void pushUp(int[] heap, int index) {
-		if (index > 0 && (heap[index] > heap[index / 2]) {
-			int value = heap[index];
-			heap[index] = heap[index / 2];
-			heap[index / 2] = value;
+		if (index > 0 && (heap[index] > heap[index / 2])) {
+			swap(heap, index, index / 2);
 			pushUp(heap, index / 2);
+		}
+	}
+
+	private static void swap(int[] heap, int ind1, int ind2) {
+		int value = heap[ind1];
+		heap[ind1] = heap[ind2];
+		heap[ind2] = value;
+	}
+
+	public static void heapify(int[] heap) {
+		int levels;
+		int exp = 0;
+		while (Math.pow(2, exp + 1) < heap.length) {
+			exp++;
+		}
+		levels = (int)Math.pow(2, exp) - 2;
+		for (int index = levels; index >= 0; index--) {
+			pushDown(heap, index);
+			for (int i : heap) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
 		}
 	}
 }
---
+++ Pile.java
@@ -0,0 +1,9 @@
+public class Pile {
+    public static void main(String[] args) {
+        int[] test1 = new int[] {-6, 5, 4, 0, 0, 3, 2, 1, -87, 32499};
+        MyHeap.heapify(test1);
+        for (int i : test1) {
+            System.out.print (i + " ");
+        }
+    }
+}
